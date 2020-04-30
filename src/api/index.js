dfef3c64a4a39393842c5b5303afebe8df743d1d
Harry Adel <harryadelb@gmail.com>
2020-04-30 17:32:34 +0200

    Setup swagger ui

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const path = require('path');
+
 const config = require('../lib/config');
 const logger = require('../lib/logger');
+const swaggerDocument = require('./swagger.json');
+
 
 const log = logger(config.logger);
 const app = express();
@@ -17,6 +22,8 @@ app.use(cookieParser());
 app.use('/v2/pet', require('./routes/pet'));
 app.use('/v2/store', require('./routes/store'));
 app.use('/v2/user', require('./routes/user'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 // catch 404
 app.use((req, res, next) => {
