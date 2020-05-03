12f2c32cf418ce1912c34dd688dce6bdd22af8ae
Harry Adel <harryadelb@gmail.com>
2020-05-03 14:53:29 +0200

    Add updateOrAdd to db operations

--- src/api/db.js
+++ src/api/db.js
@@ -6,4 +6,34 @@ const db = low(adapter);
 
 db.defaults({ pets: [], users: [], bids: [] }).write();
 
+db._.mixin({
+  updateOrAdd: function(arr, obj, arg) {
+    let item;
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arg.length; j++) {
+        if(obj[arg[j]] !== arr[i][arg[j]]){
+          item = undefined;
+          break;
+        } else {
+          item = i;
+        }
+      }
+    }
+
+    if(arr[item] === undefined){
+      arr.push(obj);
+    } else {
+      for (let key in arr[item]) {
+        if(!obj[key]){
+          delete arr[item][key];
+        }
+      }
+
+      Object.assign(arr[item], obj);
+    }
+    return arr;
+  }
+})
+
+
 module.exports = db;
