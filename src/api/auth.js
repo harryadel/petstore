1a6494904049011e485a0a6cbfab0f93295d77b2
Harry Adel <harryadelb@gmail.com>
2020-05-03 10:51:40 +0200

    Set authentication logic into its own file

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,24 @@
+const jwtExpress = require("express-jwt");
+const jwt = require("jsonwebtoken");
+const db = require("./db");
+
 const auth = {
-  required: jwt({
-    secret: "secret",
+  generateToken: ({ email, _id }) => {
+    const today = new Date();
+    const expirationDate = new Date(today);
+    expirationDate.setDate(today.getDate() + 60);
+
+    return jwt.sign(
+      {
+        email,
+        _id,
+        exp: parseInt(expirationDate.getTime() / 1000, 10),
+      },
+      process.env.JWT_SECRET
+    );
+  },
+  required: jwtExpress({
+    secret: process.env.JWT_SECRET,
     userProperty: "payload",
     getToken: (req) => {
       const {
