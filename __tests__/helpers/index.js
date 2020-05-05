ba22f0d655a803dbfbff2367af0d6bb9f2724e47
Harry Adel <harryadelb@gmail.com>
2020-05-05 13:28:00 +0200

    Extend helper with more functions

--- __tests__/helpers/index.js
+++ __tests__/helpers/index.js
@@ -1,13 +1,19 @@
 const request = require("supertest");
 const faker = require("faker");
+
 const app = require("../../src/api");
 const user = require("../../src/api/services/user");
 const db = require("../../src/api/db");
 
-const getAccessTokenAndId = async () => {
+const createAndLoginUser = async ({
+  firstName = faker.name.firstName(),
+  lastName = faker.name.lastName(),
+}) => {
+
   const fakeUser = {
     username: faker.internet.userName(),
-    firstName: faker.name.firstName(),
+    firstName,
+    lastName,
     email: faker.internet.email(),
     password: faker.internet.password(),
     phone: 1234567891111,
@@ -23,13 +29,41 @@ const getAccessTokenAndId = async () => {
   });
 };
 
+const insertPet = async ({ token }) => {
+  return await request(app)
+    .post("/v2/pet")
+    .set({ Authorization: token })
+    .send({
+      category: { name: faker.lorem.words() },
+      name: faker.name.firstName(),
+      tags: [
+        {
+          name: faker.lorem.word(),
+        },
+      ],
+      status: "Available",
+    });
+};
+
+const insertBid = async ({ token, petId, amount = faker.random.number() }) => {
+  return await request(app)
+    .post("/v2/pet/upsertBid")
+    .set({ Authorization: token })
+    .send({
+      petId,
+      amount,
+    });
+};
+
 const clearCollections = (collections) => {
-  collections.forEach(collection => {
-        db.get(collection).remove().write(); // remove users collection
+  collections.forEach((collection) => {
+    db.get(collection).remove().write(); // remove users collection
   });
-}
+};
 
 module.exports = {
-  getAccessTokenAndId,
-  clearCollections
+  createAndLoginUser,
+  clearCollections,
+  insertPet,
+  insertBid,
 };
